fead370f620b7df2a5af4d16b7dce531d0d77073
Enrico Ranieri <ran.enrico00@gmail.com>
2024-05-09 12:54:20 +0200

    aggiunge alcune query HQL ad HibernateBookRepository

--- src/main/java/org/example/model/data/repositories/abstractions/BookRepository.java
+++ src/main/java/org/example/model/data/repositories/abstractions/BookRepository.java
@@ -1,6 +1,15 @@
 package org.example.model.data.repositories.abstractions;
 
+import org.example.model.Author;
 import org.example.model.Book;
 
+import java.util.List;
+
 public interface BookRepository extends CrudRepository<Book, Integer>{
+    List<Book> findAllByNumPages(int numPages);
+    List<Book> findAllByAuthor(Author author);
+    List<Book> findAllByAuthor(int id);
+    List<Book> findAllByTitlePartAndNumPages(int numPages, String part);
+    int getBookCountByAuthorId(int id);
+    Object[] getAuthorAndBookCountById(int id);
 }
--- src/main/java/org/example/model/data/repositories/implementations/HibernateBookRepository.java
+++ src/main/java/org/example/model/data/repositories/implementations/HibernateBookRepository.java
@@ -1,5 +1,6 @@
 package org.example.model.data.repositories.implementations;
 
+import org.example.model.Author;
 import org.example.model.Book;
 import org.example.model.data.repositories.abstractions.BookRepository;
 import org.hibernate.Session;
@@ -11,7 +12,28 @@ import java.util.Optional;
 public class HibernateBookRepository implements BookRepository {
 
     private Session session;
-    
+
+    //queries
+    private static final String SELECT_BY_NUM_PAGES = """
+            SELECT b FROM Book b WHERE b.numPages = :numPages
+            """;
+    private static final String SELECT_BY_AUTHOR = """
+            SELECT b FROM Book b WHERE b.author = :author
+            """;
+    private static final String SELECT_BY_AUTHOR_ID = """
+            SELECT b FROM Book b WHERE b.author.id = :id
+            """;
+    private static final String SELECT_BY_TITLE_PART_AND_NUM_PAGES = """
+            SELECT b FROM Book b WHERE b.numPages >= :numPages AND b.title LIKE :part
+            """;
+    private static final String SELECT_COUNT_BY_AUTHOR_ID = """
+            SELECT COUNT(b) FROM Author a JOIN a.books b WHERE a.id = :id
+            """;
+    private static final String SELECT_AUTHOR_AND_BOOK_COUNT_BY_AUTHOR_ID = """
+            SELECT a, COUNT(b) FROM Author a JOIN a.books b WHERE a.id = :id
+            """;
+
+
     public HibernateBookRepository(Session session){
         this.session = session;
     }
@@ -44,5 +66,53 @@ public class HibernateBookRepository implements BookRepository {
     @Override
     public void update(Book b) {
         session.merge(b);
+    }
+
+    @Override
+    public List<Book> findAllByNumPages(int numPages) {
+        //versione formattata
+        return session.createQuery(SELECT_BY_NUM_PAGES, Book.class)
+                .setParameter("numPages", numPages)
+                .getResultList();
+//        Query<Book> query = session.createQuery(SELECT_BY_NUM_PAGES, Book.class);
+//        query.setParameter("numPages", numPages);
+//        List<Book> results = query.getResultList();
+//        return results;
+    }
+
+    @Override
+    public List<Book> findAllByAuthor(Author author) {
+        return session.createQuery(SELECT_BY_AUTHOR, Book.class)
+                .setParameter("author", author)
+                .getResultList();
+    }
+
+    @Override
+    public List<Book> findAllByAuthor(int id) {
+        return session.createQuery(SELECT_BY_AUTHOR_ID, Book.class)
+                .setParameter("id", id)
+                .getResultList();
+    }
+
+    @Override
+    public List<Book> findAllByTitlePartAndNumPages(int numPages, String part) {
+        return session.createQuery(SELECT_BY_TITLE_PART_AND_NUM_PAGES, Book.class)
+                .setParameter("numPages", numPages)
+                .setParameter("part","%"+part+"%")
+                .getResultList();
+    }
+
+    @Override
+    public int getBookCountByAuthorId(int id) {
+        return session.createQuery(SELECT_COUNT_BY_AUTHOR_ID, Integer.class)
+                .setParameter("id", id)
+                .uniqueResult();
+    }
+
+    @Override
+    public Object[] getAuthorAndBookCountById(int id) {
+        return session.createQuery(SELECT_AUTHOR_AND_BOOK_COUNT_BY_AUTHOR_ID, Object[].class)
+                .setParameter("id", id)
+                .uniqueResult();
     }
 }
